260413c0baed07ccd7a7e2a944cd3bfd92822b5f
m-miyawaki <miyawakifamilymyam@gmail.com>
2026-05-26 21:48:08 +0900

    feat: configure Capacitor and add Android platform

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,9 +1,9 @@
 import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
+  appId: 'com.example.ionicuisample',
   appName: 'ionic-ui-sample',
-  webDir: 'dist'
+  webDir: 'dist',
 };
 
 export default config;
