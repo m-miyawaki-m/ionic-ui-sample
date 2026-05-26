7582990dcf63b2f1cb5d6c56c7cfa1cc7c6ebf50
m-miyawaki <miyawakifamilymyam@gmail.com>
2026-05-26 22:05:02 +0900

    feat: add demo layout component

---
+++ src/views/components/DemoLayout.vue
@@ -0,0 +1,24 @@
+<template>
+  <ion-page>
+    <ion-header>
+      <ion-toolbar>
+        <ion-buttons slot="start">
+          <ion-back-button default-href="/components" />
+        </ion-buttons>
+        <ion-title>{{ title }}</ion-title>
+      </ion-toolbar>
+    </ion-header>
+    <ion-content class="ion-padding">
+      <slot />
+    </ion-content>
+  </ion-page>
+</template>
+
+<script setup lang="ts">
+import {
+  IonPage, IonHeader, IonToolbar, IonTitle, IonContent,
+  IonButtons, IonBackButton,
+} from '@ionic/vue';
+
+defineProps<{ title: string }>();
+</script>
